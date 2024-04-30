26ca9800ad87f885df2141b7d1433f2a0995dc0c
sabrina mendonça <sazonass.m@gmail.com>
2024-04-30 09:21:57 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
